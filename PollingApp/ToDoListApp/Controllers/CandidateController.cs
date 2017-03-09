f9984639c4530fed394a7d7fbcc1cd4943374277
Jennifer Anderson <janderson@cafepress.com>
2017-03-09 18:59:34 -0500

    Created PollingApp project and CandidateController

---
+++ PollingApp/ToDoListApp/Controllers/CandidateController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoListApp.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public string Detail()
+        {
+            return "This is the candidates Controller";
+        }
+    }
+}
